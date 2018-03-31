8870d24df8fe983ccd2f123510a917c83a3aa480
Federico Vera <fede@riddler.com.ar>
2018-03-31 19:54:00 -0300

    Extract function

--- src/main/java/net/objecthunter/exp4j/Expression.java
+++ src/main/java/net/objecthunter/exp4j/Expression.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2014 Frank Asseg
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package net.objecthunter.exp4j;
 
@@ -43,12 +43,12 @@ public class Expression {
     private final Token[] tokens;
 
     private final Set<String> userFunctionNames;
-    
+
     private final HashMap<String, VariableToken> variables = new HashMap<>(4);
 
     /**
      * Creates a new expression that is a copy of the existing one.
-     * 
+     *
      * @return copy of this {@code Expression}
      */
     public Expression copy() {
@@ -56,7 +56,7 @@ public class Expression {
             Arrays.copyOf(tokens, tokens.length),
             new HashSet<>(userFunctionNames)
         );
-        
+
         exp.variables.clear();
         //Since I don't honor the immutable token philosophy I need to copy
         //variable tokens... Still... I regret nothing!
@@ -69,10 +69,10 @@ public class Expression {
                 exp.tokens[i] = exp.variables.get(v.getName());
             }
         }
-        
+
         return exp;
     }
-    
+
     Expression(final Token[] tokens) {
         this.tokens = tokens;
         this.userFunctionNames = Collections.<String>emptySet();
@@ -92,20 +92,20 @@ public class Expression {
             }
         }
     }
-    
+
     /**
      * Sets the value of a variable, the variable to set must exist at build time and can't be the
      * name of a function.
      * All variables must be set before calling {@link Expression#evaluate()}
-     * 
+     *
      * @param name variable name as passed to {@link ExpressionBuilder}
      * @param value value of the variable
      * @return {@code this}
      * @throws IllegalArgumentException if the variable name is a function name or if the variable
      * doesn't exist at build time.
-     * @see ExpressionBuilder#build() 
-     * @see Expression#containsVariable(java.lang.String) 
-     * @see Expression#getVariableNames() 
+     * @see ExpressionBuilder#build()
+     * @see Expression#containsVariable(java.lang.String)
+     * @see Expression#getVariableNames()
      */
     public Expression setVariable(final String name, final double value) {
         this.checkVariableName(name);
@@ -124,20 +124,20 @@ public class Expression {
             throw new IllegalArgumentException(String.format("Variable '%s' doesn't exist.", name));
         }
     }
-    
+
     /**
      * Sets the value of a set of variables, the variables to set must exist at build time and can't
      * be the name of a function.
      * All variables must be set before calling {@link Expression#evaluate()}
-     * 
+     *
      * @param variables a {@code Map<String,Double>} containing all of the (name, value) pairs.
      * @return {@code this}
      * @throws IllegalArgumentException if the variable name is a function name or if the variable
      * doesn't exist at build time.
-     * @see ExpressionBuilder#build() 
-     * @see Expression#containsVariable(java.lang.String) 
-     * @see Expression#getVariableNames() 
-     * @see Expression#setVariable(java.lang.String, double) 
+     * @see ExpressionBuilder#build()
+     * @see Expression#containsVariable(java.lang.String)
+     * @see Expression#getVariableNames()
+     * @see Expression#setVariable(java.lang.String, double)
      */
     public Expression setVariables(Map<String, Double> variables) {
         for (Map.Entry<String, Double> v : variables.entrySet()) {
@@ -148,7 +148,7 @@ public class Expression {
 
     /**
      * Retrieves a {@link Set} containing all the variable names
-     * 
+     *
      * @return variable names
      */
     public Set<String> getVariableNames() {
@@ -157,7 +157,7 @@ public class Expression {
 
     /**
      * Tells if a variable exists in the expression
-     * 
+     *
      * @param name variable name
      * @return {@code true} if the variable exists and {@code false} otherwise
      */
@@ -167,8 +167,8 @@ public class Expression {
 
     /**
      * Validates an expression.<br>
-     * Building an expression is not the only metric of <i>correctness</i>, this method will 
-     * generate a {@link ValidationResult} telling if a variables are set, if the number of 
+     * Building an expression is not the only metric of <i>correctness</i>, this method will
+     * generate a {@link ValidationResult} telling if a variables are set, if the number of
      * operands is correct, and if all functions have the right number of parameters.<br><br>
      * <i><b>Note:</b></i> future version will most likely fail on build, and not at this stage
      * (at least that's my plan).
@@ -178,14 +178,7 @@ public class Expression {
      */
     public ValidationResult validate(boolean checkVariablesSet) {
         final List<String> errors = new ArrayList<>(10);
-        if (checkVariablesSet) {
-            /* check that all vars have a value set */
-            for (VariableToken vt : variables.values()) {
-                if (!vt.isValueSet()) {
-                    errors.add(String.format("The setVariable '%s' has not been set", vt.getName()));
-                }
-            }
-        }
+        checkVariablesSet(checkVariablesSet, errors);
 
         /* Check if the number of operands, functions and operators match.
            The idea is to increment a counter for operands and decrease it for operators.
@@ -202,7 +195,7 @@ public class Expression {
                     break;
                 case FUNCTION:
                     final Function func = ((FunctionToken) tok).getFunction();
-                    final int argsNum = func.getNumArguments(); 
+                    final int argsNum = func.getNumArguments();
                     if (argsNum > count) {
                         errors.add(String.format("Not enough arguments for '%s'", func.getName()));
                     }
@@ -225,6 +218,7 @@ public class Expression {
                 return new ValidationResult(errors);
             }
         }
+
         if (count > 1) {
             errors.add("Too many operands");
         }
@@ -234,7 +228,7 @@ public class Expression {
 
     /**
      * Alias for {@code Expression#validate(true)}
-     * 
+     *
      * @return {@link ValidationResult}
      * @see Expression#validate(boolean)
      */
@@ -259,15 +253,15 @@ public class Expression {
     }
 
     /**
-     * Evaluates the expression with the given values, this method will fail if 
-     * {@link Expression#validate()} returns a {@link ValidationResult} different that 
+     * Evaluates the expression with the given values, this method will fail if
+     * {@link Expression#validate()} returns a {@link ValidationResult} different that
      * {@link ValidationResult#SUCCESS}.<br><br>
-     * <i><b>Note:</b></i> future version will most likely fail on build, and not at this stage, 
+     * <i><b>Note:</b></i> future version will most likely fail on build, and not at this stage,
      * this method will only fail if variables aren't set.
-     * 
+     *
      * @return result of the evaluation
      * @throws IllegalArgumentException if the expression isn't valid
-     * @see Expression#validate() 
+     * @see Expression#validate()
      */
     public double evaluate() {
         final ArrayStack output = new ArrayStack();
@@ -289,7 +283,7 @@ public class Expression {
                         "Invalid number of operands available for '%s' operator", op.getSymbol()
                     ));
                 }
-                
+
                 if (op.getNumOperands() == 2) {
                     /* pop the operands and push the result of the operation */
                     double rightArg = output.pop();
@@ -303,57 +297,71 @@ public class Expression {
             } else if (t.getType() == FUNCTION) {
                 final Function func = ((FunctionToken) t).getFunction();
                 final int numArguments = func.getNumArguments();
-                
+
                 if (output.size() < numArguments) {
                     throw new IllegalArgumentException(String.format(
                         "Invalid number of arguments available for '%s' function", func.getName()
                     ));
                 }
-                
+
                 /* collect the arguments from the stack */
                 final double[] args = new double[numArguments];
                 for (int j = numArguments - 1; j >= 0; j--) {
                     args[j] = output.pop();
                 }
-                
+
                 output.push(func.apply(args));
             }
         }
-        
+
         if (output.size() > 1) {
             throw new IllegalArgumentException(
                     "Invalid number of items on the output queue. "
                   + "Might be caused by an invalid number of arguments for a function."
             );
         }
-        
+
         return output.pop();
     }
-    
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(tokens.length * 15);
-        
+
         for (Token token : tokens) {
             sb.append(token).append(' ');
         }
-        
+
         return sb.substring(0, sb.length() - 1);
     }
-    
+
     /**
      * Retrieves the internal representation of the expression.<br>
      * This method is mostly useless for most users.
-     * 
+     *
      * @return RPN of the expression
      */
     public String toTokenString() {
         StringBuilder sb = new StringBuilder(tokens.length * 35);
-        
+
         for (Token token : tokens) {
             sb.append(token.getType()).append('[').append(token).append("] ");
         }
-        
+
         return sb.substring(0, sb.length() - 1);
+    }
+
+    private void checkVariablesSet(boolean checkVariablesSet, List<String> errors) {
+        if (checkVariablesSet) {
+            /* check that all vars have a value set */
+            for (VariableToken vt : variables.values()) {
+                if (!vt.isValueSet()) {
+                    errors.add(String.format(
+                            "The setVariable '%s' has not been set",
+                            vt.getName())
+                    );
+                }
+            }
+        }
     }
 }
