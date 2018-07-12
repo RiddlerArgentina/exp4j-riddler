fcc2589e815a5387a13c4d5b96f1c23e63e365b8
Federico Vera <fede@riddler.com.ar>
2018-07-12 08:37:24 -0300

    Make Funtions static inner classes instead of Anonymous Inner Clasess Task #757 - Don't use Anonymous Inner Classes

--- src/main/java/net/objecthunter/exp4j/function/Functions.java
+++ src/main/java/net/objecthunter/exp4j/function/Functions.java
@@ -15,215 +15,66 @@
 */
 package net.objecthunter.exp4j.function;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
 /**
  * Class representing the builtin functions available for use in expressions
  */
 public final class Functions {
-    public static final Function SIN;
-    public static final Function COS;
-    public static final Function TAN;
-    public static final Function LOG;
-    public static final Function LOG1P;
-    public static final Function ABS;
-    public static final Function ACOS;
-    public static final Function ASIN;
-    public static final Function ATAN;
-    public static final Function CBRT;
-    public static final Function CEIL;
-    public static final Function FLOOR;
-    public static final Function SINH;
-    public static final Function SQRT;
-    public static final Function TANH;
-    public static final Function COSH ;
-    public static final Function POW;
-    public static final Function EXP;
-    public static final Function EXPM1;
-    public static final Function LOG10;
-    public static final Function LOG2;
-    public static final Function SGN;
-    public static final Function PI;
-    public static final Function E;
+    public static final Function SIN   = new Sin();
+    public static final Function COS   = new Cos();
+    public static final Function TAN   = new Tan();
+    public static final Function LOG   = new Log();
+    public static final Function LOG1P = new Log1p();
+    public static final Function LOG10 = new Log10();
+    public static final Function LOG2  = new Log2();
+    public static final Function ABS   = new Abs();
+    public static final Function ACOS  = new ACos();
+    public static final Function ASIN  = new ASin();
+    public static final Function ATAN  = new ATan();
+    public static final Function CBRT  = new CBRT();
+    public static final Function CEIL  = new Ceil();
+    public static final Function FLOOR = new Floor();
+    public static final Function SINH  = new Sinh();
+    public static final Function SQRT  = new Sqrt();
+    public static final Function TANH  = new Tanh();
+    public static final Function COSH  = new Cosh();
+    public static final Function POW   = new Pow();
+    public static final Function EXP   = new Exp();
+    public static final Function EXPM1 = new Expm1();
+    public static final Function SGN   = new Signum();
+    public static final Function PI    = new Pi();
+    public static final Function E     = new E();
 
     private static final Function[] BUILTIN = new Function[24];
 
     static {
         int i = 0;
-        SIN = new Function("sin") {
-            @Override
-            public double apply(double... args) {
-                return Math.sin(args[0]);
-            }
-        };
         BUILTIN[i++] = SIN;
-        COS = new Function("cos") {
-            @Override
-            public double apply(double... args) {
-                return Math.cos(args[0]);
-            }
-        };
         BUILTIN[i++] = COS;
-        TAN = new Function("tan") {
-            @Override
-            public double apply(double... args) {
-                return Math.tan(args[0]);
-            }
-        };
         BUILTIN[i++] = TAN;
-        LOG = new Function("log") {
-            @Override
-            public double apply(double... args) {
-                return Math.log(args[0]);
-            }
-        };
         BUILTIN[i++] = LOG;
-        LOG2 = new Function("log2") {
-            @Override
-            public double apply(double... args) {
-                return Math.log(args[0]) / Math.log(2d);
-            }
-        };
         BUILTIN[i++] = LOG2;
-        LOG10 = new Function("log10") {
-            @Override
-            public double apply(double... args) {
-                return Math.log10(args[0]);
-            }
-        };
         BUILTIN[i++] = LOG10;
-        LOG1P = new Function("log1p") {
-            @Override
-            public double apply(double... args) {
-                return Math.log1p(args[0]);
-            }
-        };
         BUILTIN[i++] = LOG1P;
-        ABS = new Function("abs") {
-            @Override
-            public double apply(double... args) {
-                return Math.abs(args[0]);
-            }
-        };
         BUILTIN[i++] = ABS;
-        ACOS = new Function("acos") {
-            @Override
-            public double apply(double... args) {
-                return Math.acos(args[0]);
-            }
-        };
         BUILTIN[i++] = ACOS;
-        ASIN = new Function("asin") {
-            @Override
-            public double apply(double... args) {
-                return Math.asin(args[0]);
-            }
-        };
         BUILTIN[i++] = ASIN;
-        ATAN = new Function("atan") {
-            @Override
-            public double apply(double... args) {
-                return Math.atan(args[0]);
-            }
-        };
         BUILTIN[i++] = ATAN;
-        CBRT = new Function("cbrt") {
-            @Override
-            public double apply(double... args) {
-                return Math.cbrt(args[0]);
-            }
-        };
         BUILTIN[i++] = CBRT;
-        FLOOR = new Function("floor") {
-            @Override
-            public double apply(double... args) {
-                return Math.floor(args[0]);
-            }
-        };
         BUILTIN[i++] = FLOOR;
-        SINH = new Function("sinh") {
-            @Override
-            public double apply(double... args) {
-                return Math.sinh(args[0]);
-            }
-        };
-        BUILTIN[i++] = SINH;
-        SQRT = new Function("sqrt") {
-            @Override
-            public double apply(double... args) {
-                return Math.sqrt(args[0]);
-            }
-        };
-        BUILTIN[i++] = SQRT;
-        TANH = new Function("tanh") {
-            @Override
-            public double apply(double... args) {
-                return Math.tanh(args[0]);
-            }
-        };
-        BUILTIN[i++] = TANH;
-        COSH = new Function("cosh") {
-            @Override
-            public double apply(double... args) {
-                return Math.cosh(args[0]);
-            }
-        };
-        BUILTIN[i++] = COSH;
-        CEIL = new Function("ceil") {
-            @Override
-            public double apply(double... args) {
-                return Math.ceil(args[0]);
-            }
-        };
         BUILTIN[i++] = CEIL;
-        POW = new Function("pow", 2) {
-            @Override
-            public double apply(double... args) {
-                return Math.pow(args[0], args[1]);
-            }
-        };
+        BUILTIN[i++] = SINH;
+        BUILTIN[i++] = COSH;
+        BUILTIN[i++] = TANH;
+        BUILTIN[i++] = SQRT;
         BUILTIN[i++] = POW;
-        EXP = new Function("exp", 1) {
-            @Override
-            public double apply(double... args) {
-                return Math.exp(args[0]);
-            }
-        };
         BUILTIN[i++] = EXP;
-        EXPM1 = new Function("expm1", 1) {
-            @Override
-            public double apply(double... args) {
-                return Math.expm1(args[0]);
-            }
-        };
         BUILTIN[i++] = EXPM1;
-        SGN = new Function("signum", 1) {
-            @Override
-            public double apply(double... args) {
-                if (args[0] > 0) {
-                    return 1;
-                } else if (args[0] < 0) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }
-        };
         BUILTIN[i++] = SGN;
-        PI = new Function("pi", 0) {
-            @Override
-            public double apply(double... args) {
-                return Math.PI;
-            }
-        };
-        BUILTIN[i++] = PI;
-        E = new Function("e", 0) {
-            @Override
-            public double apply(double... args) {
-                return Math.E;
-            }
-        };
         BUILTIN[i++] = E;
+        BUILTIN[i++] = PI;
     }
 
     private Functions() {
@@ -272,6 +123,204 @@ public final class Functions {
             case "e":     return E;
             default:
                 return null;
+        }
+    }
+
+    private static final class Sin extends Function implements Serializable {
+        Sin() { super("sin"); }
+        @Override
+        public double apply(double... args) {
+            return Math.sin(args[0]);
+        }
+    }
+
+    private static final class Cos extends Function implements Serializable {
+        Cos() { super("cos"); }
+        @Override
+        public double apply(double... args) {
+            return Math.cos(args[0]);
+        }
+    }
+
+    private static final class Tan extends Function implements Serializable {
+        Tan() { super("tan"); }
+        @Override
+        public double apply(double... args) {
+            return Math.tan(args[0]);
+        }
+    }
+
+    private static final class Log extends Function implements Serializable {
+        Log() { super("log"); }
+        @Override
+        public double apply(double... args) {
+            return Math.log(args[0]);
+        }
+    }
+
+    private static final class Log2 extends Function implements Serializable {
+        Log2() { super("log2"); }
+        @Override
+        public double apply(double... args) {
+            return Math.log(args[0]) / Math.log(2d);
+        }
+    }
+
+    private static final class Log10 extends Function implements Serializable {
+        Log10() { super("log10"); }
+        @Override
+        public double apply(double... args) {
+            return Math.log10(args[0]);
+        }
+    }
+
+    private static final class Log1p extends Function implements Serializable {
+        Log1p() { super("log1p"); }
+        @Override
+        public double apply(double... args) {
+            return Math.log1p(args[0]);
+        }
+    }
+
+    private static final class Abs extends Function implements Serializable {
+        Abs() { super("abs"); }
+        @Override
+        public double apply(double... args) {
+            return Math.abs(args[0]);
+        }
+    }
+
+    private static final class ACos extends Function implements Serializable {
+        ACos() { super("acos"); }
+        @Override
+        public double apply(double... args) {
+            return Math.acos(args[0]);
+        }
+    }
+
+    private static final class ASin extends Function implements Serializable {
+        ASin() { super("asin"); }
+        @Override
+        public double apply(double... args) {
+            return Math.asin(args[0]);
+        }
+    }
+
+    private static final class ATan extends Function implements Serializable {
+        ATan() { super("atan"); }
+        @Override
+        public double apply(double... args) {
+            return Math.atan(args[0]);
+        }
+    }
+
+    private static final class CBRT extends Function implements Serializable {
+        CBRT() { super("cbrt"); }
+        @Override
+        public double apply(double... args) {
+            return Math.cbrt(args[0]);
+        }
+    }
+
+    private static final class Floor extends Function implements Serializable {
+        Floor() { super("floor"); }
+        @Override
+        public double apply(double... args) {
+            return Math.floor(args[0]);
+        }
+    }
+
+    private static final class Sinh extends Function implements Serializable {
+        Sinh() { super("sinh"); }
+        @Override
+        public double apply(double... args) {
+            return Math.sinh(args[0]);
+        }
+    }
+
+    private static final class Tanh extends Function implements Serializable {
+        Tanh() { super("tanh"); }
+        @Override
+        public double apply(double... args) {
+            return Math.tanh(args[0]);
+        }
+    }
+
+    private static final class Cosh extends Function implements Serializable {
+        Cosh() { super("cosh"); }
+        @Override
+        public double apply(double... args) {
+            return Math.cosh(args[0]);
+        }
+    }
+
+    private static final class Ceil extends Function implements Serializable {
+        Ceil() { super("ceil"); }
+        @Override
+        public double apply(double... args) {
+            return Math.ceil(args[0]);
+        }
+    }
+
+    private static final class Sqrt extends Function implements Serializable {
+        Sqrt() { super("sqrt"); }
+        @Override
+        public double apply(double... args) {
+            return Math.sqrt(args[0]);
+        }
+    }
+
+    private static final class Pow extends Function implements Serializable {
+        Pow() { super("pow", 2); }
+        @Override
+        public double apply(double... args) {
+            return Math.pow(args[0], args[1]);
+        }
+    }
+
+    private static final class Exp extends Function implements Serializable {
+        Exp() { super("exp"); }
+        @Override
+        public double apply(double... args) {
+            return Math.exp(args[0]);
+        }
+    }
+
+    private static final class Expm1 extends Function implements Serializable {
+        Expm1() { super("expm1"); }
+        @Override
+        public double apply(double... args) {
+            return Math.expm1(args[0]);
+        }
+    }
+
+    private static final class Signum extends Function implements Serializable {
+        Signum() { super("signum"); }
+        @Override
+        public double apply(double... args) {
+            if (args[0] > 0) {
+                return 1;
+            } else if (args[0] < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    private static final class Pi extends Function implements Serializable {
+        Pi () { super("pi", 0); }
+        @Override
+        public double apply(double... args) {
+            return Math.PI;
+        }
+    }
+
+    private static final class E extends Function implements Serializable {
+        E() { super("e", 0); }
+        @Override
+        public double apply(double... args) {
+            return Math.E;
         }
     }
 }
