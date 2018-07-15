a57067ce7dc0b73c05df6e3f117997ae6b29a6a4
Federico Vera <fede@riddler.com.ar>
2018-07-15 07:01:50 +0000

    Fix a typo in the docs

--- src/main/java/net/objecthunter/exp4j/extras/FunctionsBoolean.java
+++ src/main/java/net/objecthunter/exp4j/extras/FunctionsBoolean.java
@@ -67,10 +67,10 @@ public final class FunctionsBoolean {
      * <pre>
      *     <b>a</b> | <b>b</b> | <b>a|b</b>
      *     --+---+----
-     *     0 | 0 |  <i>1</i>
+     *     0 | 0 |  <i>0</i>
      *     0 | 1 |  <i>1</i>
      *     1 | 0 |  <i>1</i>
-     *     1 | 1 |  <i>0</i>
+     *     1 | 1 |  <i>1</i>
      * </pre>
      */
     public static final Function OR    = new Or();
