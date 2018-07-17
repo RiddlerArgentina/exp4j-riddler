2deaee43a1326b2c9a6035b5241dd73f133904e0
Federico Vera <fede@riddler.com.ar>
2018-07-17 11:18:01 -0300

    Add tests for expression based functions (Task #784)

---
+++ src/test/java/net/objecthunter/exp4j/FunctionExpresionTest.java
@@ -0,0 +1,116 @@
+/*
+* Copyright 2018 Federico Vera
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package net.objecthunter.exp4j;
+
+import net.objecthunter.exp4j.extras.FunctionExpresion;
+import net.objecthunter.exp4j.extras.FunctionsMisc;
+import net.objecthunter.exp4j.extras.OperatorsComparison;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Federico Vera {@literal <fede@riddler.com.ar>}
+ */
+public class FunctionExpresionTest {
+    @Test
+    public void testFunctionExpression() {
+        FunctionExpresion fe = new FunctionExpresion("foo", 2, "a + b");
+        Expression e = new ExpressionBuilder("foo(1, 2)")
+                            .function(fe)
+                            .build();
+        assertEquals(3, e.evaluate(), 0d);
+    }
+
+    @Test
+    public void testFunctionExpression2() {
+        FunctionExpresion fe = new FunctionExpresion("foo", 2, "a + b");
+        Expression e = new ExpressionBuilder("foo(1, 2)")
+                            .function(fe)
+                            .build(true);
+        assertEquals(3, e.evaluate(), 0d);
+    }
+
+    @Test
+    public void testFunctionExpression3() {
+        FunctionExpresion fe = new FunctionExpresion(
+                "foo", 3, true,
+                "if((a < b) & (b < c), 2 * b, c)",
+                FunctionsMisc.getFunctions(),
+                OperatorsComparison.getOperators()
+        );
+        Expression e = new ExpressionBuilder("foo(1, 2, 3)")
+                            .function(fe)
+                            .build();
+        assertEquals(4, e.evaluate(), 0d);
+    }
+
+    @Test
+    public void testFunctionExpression4() {
+        FunctionExpresion fe = new FunctionExpresion(
+                "foo", 3, true,
+                "if((a < b) & (b < c), 2 * b, c)",
+                FunctionsMisc.getFunctions(),
+                OperatorsComparison.getOperators()
+        );
+        Expression e = new ExpressionBuilder("foo(3, 2, 1)")
+                            .function(fe)
+                            .build();
+        assertEquals(1, e.evaluate(), 0d);
+    }
+
+    @Test
+    public void testFunctionExpression5() {
+        FunctionExpresion fe = new FunctionExpresion(
+                "foo", 3, true,
+                "a + b - c",
+                FunctionsMisc.getFunctions(),
+                OperatorsComparison.getOperators()
+        );
+        Expression e = new ExpressionBuilder("foo(3, 2, foo(3, 2, 1)) * foo(3, 2, 1)")
+                            .function(fe)
+                            .build();
+        assertEquals(4, e.evaluate(), 0d);
+    }
+
+    @Test
+    public void testFunctionExpression6() {
+        FunctionExpresion fe = new FunctionExpresion(
+                "foo", 3, true,
+                "a + b - c",
+                FunctionsMisc.getFunctions(),
+                OperatorsComparison.getOperators()
+        );
+        Expression e = new ExpressionBuilder("foo(3, 2, foo(3, 2, 1)) * foo(3, 2, 1)")
+                            .function(fe)
+                            .build(true);
+        assertEquals(4, e.evaluate(), 0d);
+    }
+
+    @Test
+    public void testFunctionExpression7() {
+        FunctionExpresion fe = new FunctionExpresion(
+                "foo", 2,
+                "a * lcm(a, b)",
+                FunctionsMisc.getFunctions()
+        );
+        Expression e = new ExpressionBuilder("foo(3, 5)")
+                            .function(fe)
+                            .build(true);
+        assertEquals(45, e.evaluate(), 0d);
+    }
+}
